7a788406d2e49d731305beff5bc4402cfd426467
Casey Waweru <casey.waweru@gmail.com>
2026-06-25 18:38:20 +0300

    feat: adding scripts to install basic packages

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+set -e
 
 # configure it to check the OS and update accordingly
 sudo pacman -Syu
 
+# more pre-requisites
+sudo pamcan -S --needed git base-devel
+
 # install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 # add packages
+yay git neovim curl tmux ghostty
 
 # setup configs using dotfiles
 
